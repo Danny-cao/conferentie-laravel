7f417e4be4ead02b5c2b820830042595fb5f749e
Danny <danny.dc.cao@gmail.com>
2016-11-10 19:50:01 +0000

    added seeders ready for seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        
+        
+       $this->call(zaalTableSeeder::class);    
+       
+       $this->call(WensenTableSeeder::class);    
+       
+       $this->call(RoleTableSeeder::class);   
+       
+       $this->call(UserTableSeeder::class);    
+       
+       $this->call(TicketTypeTableSeeder::class);    
+       
+       $this->call(MaaltijdTypeTableSeeder::class);
+        
+       $this->call(StatusTableSeeder::class);    
+       
+       $this->call(AgendaTableSeeder::class);   
+       
+       $this->call(ZaalTableSeeder::class);
+       
        $this->call(TagTableSeeder::class);
+       
+       $this->call(SlotTableSeeder::class);
+        
+       
     }
 }
